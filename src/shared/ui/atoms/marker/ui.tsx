41a7dce1b6384d55c4e33a4e0175dcc2e3c812a5
AgentosUA <oleh.stepaniuk@chatbots.studio>
2024-02-14 00:43:53 +0200

    wip: marker colors

--- src/shared/ui/atoms/marker/ui.tsx
+++ src/shared/ui/atoms/marker/ui.tsx
@@ -3,13 +3,16 @@ import { Icon } from 'leaflet';
 import { FC, PropsWithChildren, useMemo, useRef } from 'react';
 import { Marker } from 'react-leaflet';
 
+// import styles from './ui.module.scss';
+
 const MarkerIcons = markerNames.map(
   (markerName) =>
     new Icon({
       iconUrl: `/markers/${markerName}.png`,
       iconSize: [32, 32], // size of the icon
       iconAnchor: [22, 22], // point of the icon which will correspond to marker's location
-      popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
+      popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor,
+      // className: styles.blue,
     })
 );
 
