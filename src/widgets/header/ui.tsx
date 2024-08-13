18f978acb9c2ff2d724e5d0e6f3771107d59a271
AgentosUA <agentoswork@gmail.com>
2024-08-14 00:11:35 +0300

    feat: update header

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,42 +1,76 @@
+import { useEffect, useState } from 'react';
+
 import Image from 'next/image';
-import styles from './ui.module.scss';
-import { Button } from '@/shared/ui/atoms/button';
+
 import Link from 'next/link';
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.menu}>
-      <Link href='https://savelife.in.ua/' target='_blank'>
-        <Button className={styles.menuItem} variant='transparent'>
-          SUPPORT
-        </Button>
-      </Link>
-    </div>
-    <div className={styles.logo}>
-      <Link href='/'>
-        <Image
-          className={styles.logoImage}
-          src='/a3-logo.png'
-          width={159}
-          height={91}
-          alt='logo'
-        />
-        <h1 className={styles.title}>PLAN MAKER ONLINE</h1>
-      </Link>
-    </div>
-    <div className={styles.menu}>
-      <Link href='/auth/login'>
-        <Button className={styles.menuItem} size='md' variant='transparent'>
-          LOG IN
-        </Button>
-      </Link>
-      <Link href='/auth/sign-up'>
-        <Button className={styles.menuItem} variant='transparent'>
-          SIGN UP
-        </Button>
-      </Link>
-    </div>
-  </header>
-);
+import { Button } from '@/shared/ui/atoms/button';
+
+import styles from './ui.module.scss';
+import classNames from 'classnames';
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header
+      className={classNames(styles.header, {
+        [styles.scrolledHeader]: isScrolled,
+      })}>
+      <div className={styles.menu}>
+        <Link href='https://savelife.in.ua/' target='_blank'>
+          <Button className={styles.menuItem} variant='transparent'>
+            SUPPORT
+          </Button>
+        </Link>
+        <Link href='/changelog'>
+          <Button className={styles.menuItem} variant='transparent'>
+            Changelog
+          </Button>
+        </Link>
+      </div>
+      <div className={styles.logo}>
+        <Link href='/'>
+          <Image
+            className={styles.logoImage}
+            src='/a3-logo.png'
+            width={159}
+            height={91}
+            alt='logo'
+          />
+          <h1 className={styles.title}>PLAN MAKER ONLINE</h1>
+        </Link>
+      </div>
+      <div className={styles.menu}>
+        <Link href='/auth/login'>
+          <Button className={styles.menuItem} size='md' variant='transparent'>
+            LOG IN
+          </Button>
+        </Link>
+        <Link href='/auth/sign-up'>
+          <Button className={styles.menuItem} variant='transparent'>
+            SIGN UP
+          </Button>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export { Header };
