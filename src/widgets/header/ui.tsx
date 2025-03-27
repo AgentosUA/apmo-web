f3a2a9ad33594bcc4fcbafb094411d73c8b2fb55
Oleh Stepaniuk <oleh.stepaniuk@chatbots.studio>
2025-03-28 00:01:25 +0200

    fix: logo

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -45,17 +45,18 @@ const Header = () => {
         {
           ['duration-300 transition-all bg-black shadow-sm']: isScrolled,
         }
-      )}>
+      )}
+    >
       {isMenuOpened ? (
         <IoMdClose
-          className='absolute top-1/2 left-4 text-[25px] z-[11] transform -translate-y-1/2 lg:hidden'
-          color='#fff'
+          className="absolute top-1/2 left-4 text-[25px] z-[11] transform -translate-y-1/2 lg:hidden"
+          color="#fff"
           onClick={onBurgerMenuClick}
         />
       ) : (
         <RxHamburgerMenu
-          className='absolute top-1/2 left-4 text-[25px] z-[11] transform -translate-y-1/2 lg:hidden'
-          color='#fff'
+          className="absolute top-1/2 left-4 text-[25px] z-[11] transform -translate-y-1/2 lg:hidden"
+          color="#fff"
           onClick={onBurgerMenuClick}
         />
       )}
@@ -63,70 +64,78 @@ const Header = () => {
       <div
         className={classNames('flex flex-start items-center w-full h-8 bg-black px-4', {
           'left-0 transition-all left duration-300': isMenuOpened,
-        })}>
+        })}
+      >
         <Link
           className={styles.menuItem}
-          href='https://savelife.in.ua/en/donate-en/'
-          target='_blank'>
-          <Button className='' variant='transparent'>
-            <Localize translationKey='widgets:header:support' />
+          href="https://savelife.in.ua/en/donate-en/"
+          target="_blank"
+        >
+          <Button className="" variant="transparent">
+            <Localize translationKey="widgets:header:support" />
           </Button>
         </Link>
-        <Link className={classNames(styles.menuItem)} href='/changelog'>
-          <Button className='' variant='transparent'>
-            <Localize translationKey='widgets:header:changelog' />
+        <Link className={classNames(styles.menuItem)} href="/changelog">
+          <Button className="" variant="transparent">
+            <Localize translationKey="widgets:header:changelog" />
           </Button>
         </Link>
-        <div className='grow-[1]' />
+        <div className="grow-[1]" />
         <UnAuthorized>
-          <Link className='max-lg:text-center max-lg:w-full' href='/auth/login'>
-            <Button
-              className='text-center w-full '
-              size='md'
-              variant='transparent'>
-              <Localize translationKey='widgets:header:logIn' />
+          <Link className="max-lg:text-center max-lg:w-full" href="/auth/login">
+            <Button className="text-center w-full " size="md" variant="transparent">
+              <Localize translationKey="widgets:header:logIn" />
             </Button>
           </Link>
-          <Link
-            className='max-lg:text-center max-lg:w-full'
-            href='/auth/sign-up'>
-            <Button className='text-center w-full ' variant='transparent'>
-              <Localize translationKey='widgets:header:signUp' />
+          <Link className="max-lg:text-center max-lg:w-full" href="/auth/sign-up">
+            <Button className="text-center w-full " variant="transparent">
+              <Localize translationKey="widgets:header:signUp" />
             </Button>
           </Link>
         </UnAuthorized>
 
         <Authorized>
-          <Link className='max-lg:text-center max-lg:w-full' href='/profile'>
-            <Button className='' size='md' variant='transparent'>
-              <Localize translationKey='widgets:header:profile' />
+          <Link className="max-lg:text-center max-lg:w-full" href="/profile">
+            <Button className="" size="md" variant="transparent">
+              <Localize translationKey="widgets:header:profile" />
             </Button>
           </Link>
 
-          <Link className='max-lg:text-center max-lg:w-full' href='/'>
-            <Button
-              className=''
-              size='md'
-              variant='transparent'
-              onClick={userEntity.logout}>
-              <Localize translationKey='widgets:header:logOut' />
+          <Link className="max-lg:text-center max-lg:w-full" href="/">
+            <Button className="" size="md" variant="transparent" onClick={userEntity.logout}>
+              <Localize translationKey="widgets:header:logOut" />
             </Button>
           </Link>
         </Authorized>
       </div>
       <div
-        className={classNames('absolute top-[5px] left-1/2 transform -translate-x-1/2 text-center duration-300 flex items-center z-10 justify-center', {
-          ['top-[2px] text-[16px]']: isScrolled,
-        })}>
-        <Link href='/'>
+        className={classNames(
+          'absolute top-[5px] left-1/2 transform -translate-x-1/2 text-center duration-300 flex items-center z-10 justify-center',
+          {
+            ['top-[2px] text-[16px]']: isScrolled,
+          }
+        )}
+      >
+        <Link className="flex flex-col items-center" href="/">
           <Image
-            className={styles.logoImage}
-            src='/a3-logo.png'
+            className={classNames('duration-300 transition-all', {
+              'flex items-center justify-center h-16 w-auto text-center mx-auto': isScrolled,
+            })}
+            src="/a3-logo.png"
             width={159}
             height={91}
-            alt='logo'
+            alt="logo"
           />
-          <h1 className={styles.title}>PLAN MAKER ONLINE</h1>
+          <h1
+            className={classNames(
+              'mt-[-3px] text-[20px] font-medium text-white text-center transition duration-300 mx-auto',
+              {
+                'text-[16px]': isScrolled,
+              }
+            )}
+          >
+            PLAN MAKER ONLINE
+          </h1>
         </Link>
       </div>
     </header>
